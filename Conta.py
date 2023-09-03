15addc3a49b9498f5d3611a8c0f9c38bd9f4670d
natancolen <natancolen@gmail.com>
2023-09-02 21:16:10 -0300

    criando metodos

--- Conta.py
+++ Conta.py
@@ -5,3 +5,13 @@ class Conta:
         self.titular = titular
         self.saldo = saldo
         self.limite = limite
+
+    def extrato(self):
+        print("Saldo de {} do titular {}".format(self.saldo, self.titular))
+
+    def deposita(self, valor):
+        print("Depositando...".format(self))
+        self.saldo += valor
+    def sacar(self, valor):
+        print("Sacando o dinheiro...".format(self))
+        self.saldo -= valor
