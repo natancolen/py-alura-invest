0f5ada17d5b0d7e6e87509137e57f9ed81cf54b7
natancolen <natancolen@gmail.com>
2023-09-07 00:00:11 -0300

    Tornando os atributos privados

--- Conta.py
+++ Conta.py
@@ -1,17 +1,15 @@
 class Conta:
     def __init__(self, conta, titular, saldo, limite):
         print('Criando a conta...'.format(self))
-        self.conta = conta
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__conta = conta
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     def extrato(self):
-        print("Saldo de {} do titular {}".format(self.saldo, self.titular))
+        print("Saldo de {} do titular {}".format(self.__saldo, self.__titular))
 
     def deposita(self, valor):
-        print("Depositando...".format(self))
-        self.saldo += valor
+        self.__saldo += valor
     def sacar(self, valor):
-        print("Sacando o dinheiro...".format(self))
-        self.saldo -= valor
+        self.__saldo -= valor
